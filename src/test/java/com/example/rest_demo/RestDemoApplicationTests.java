170e6cf5eb97eaff15d3c1ed1bdc3ab2d83dcb66
Icebergik <raseda.khatoon@gmail.com>
2026-04-05 15:42:36 +0530

    test case commented

--- src/test/java/com/example/rest_demo/RestDemoApplicationTests.java
+++ src/test/java/com/example/rest_demo/RestDemoApplicationTests.java
@@ -1,12 +1,16 @@
 package com.example.rest_demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
-@SpringBootTest(classes = RestDemoApplication.class)
-@ComponentScan("com.example.rest_demo")
+//@SpringBootTest(classes = RestDemoApplication.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = RestDemoApplication.class)
 class RestDemoApplicationTests {
 
 	@Test
